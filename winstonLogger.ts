6b6e76750ec1ebdd530abc36c2d9148c7b42be6c
alex olivares <alexander.b.olivares@gmail.com>
2023-07-19 18:08:20 -0500

    Test action network request

--- winstonLogger.ts
+++ winstonLogger.ts
@@ -1,19 +1,13 @@
 import dotenv from "dotenv";
 import winston from "winston";
 import { WinstonTransport as AxiomTransport } from "@axiomhq/axiom-node";
+import axios from "axios";
 
 dotenv.config();
 
 const logger = winston.createLogger({
   level: "http",
   format: winston.format.json(),
-  transports: [
-    new AxiomTransport({
-      dataset: process.env.AXIOM_DATASET,
-      token: process.env.AXIOM_TOKEN,
-      orgId: process.env.AXIOM_ORG_ID,
-    }),
-  ],
 });
 
 if (process.env.NODE_ENV !== "production") {
@@ -24,17 +18,31 @@ if (process.env.NODE_ENV !== "production") {
   );
 }
 
-// if (process.env.NODE_ENV === "production") {
-//   console.log("in production block of winstonLogger.ts");
+if (process.env.NODE_ENV === "production") {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
 
-//   logger.add(
-//     new AxiomTransport({
-//       dataset: process.env.AXIOM_DATASET,
-//       token: process.env.AXIOM_TOKEN,
-//       orgId: process.env.AXIOM_ORG_ID,
-//     })
-//   );
-// }
+  async function makeRequest() {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  makeRequest();
+
+  logger.add(
+    new AxiomTransport({
+      dataset: process.env.AXIOM_DATASET,
+      token: process.env.AXIOM_TOKEN,
+      orgId: process.env.AXIOM_ORG_ID,
+      level: "debug",
+    })
+  );
+}
 
 logger.log({
   level: "info",
