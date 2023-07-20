2a0b3e2c28b7dbb8a9f26b136ff1dd4c7487568e
alex olivares <alexander.b.olivares@gmail.com>
2023-07-19 21:59:19 -0500

    new logging config test

--- winstonLogger.ts
+++ winstonLogger.ts
@@ -1,37 +1,30 @@
 import dotenv from "dotenv";
 import winston from "winston";
 import { WinstonTransport as AxiomTransport } from "@axiomhq/axiom-node";
-import util from "util";
 
 dotenv.config();
+
+const axiomTransport = new AxiomTransport({});
 
 const logger = winston.createLogger({
   level: "http",
   format: winston.format.json(),
-  transports: [
-    new AxiomTransport({
-      dataset: process.env.AXIOM_DATASET,
-      token: process.env.AXIOM_TOKEN,
-      orgId: process.env.AXIOM_ORG_ID,
-    }),
-  ],
+  transports: [axiomTransport],
+  exceptionHandlers: [axiomTransport],
+  rejectionHandlers: [axiomTransport],
 });
 
-// if (process.env.NODE_ENV !== "production") {
-//   logger.add(
-//     new winston.transports.Console({
-//       format: winston.format.simple(),
-//     })
-//   );
-// }
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 
-// if (process.env.NODE_ENV !== "production") {
-//   logger.add(new AxiomTransport({}));
-// }
-
-logger.transports.forEach(t => {
-  console.log(util.inspect(t, { depth: null }));
-});
+if (process.env.NODE_ENV === "production") {
+  logger.add(new AxiomTransport({}));
+}
 
 // logger.error({
 //   level: "error",
@@ -39,14 +32,17 @@ logger.transports.forEach(t => {
 // });
 logger.warn({
   level: "warn",
-  message: "from gh actions!",
+  message: "new transport config",
 });
 logger.info({
   level: "info",
+  message: "new transport config",
 });
 logger.http({
   level: "http",
-  message: "from gh actions!",
+  message: "new transport config",
 });
+
+throw new Error("alex threw this uncaught error");
 
 export default logger;
