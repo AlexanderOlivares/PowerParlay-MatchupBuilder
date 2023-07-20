15f42630cfc7c1aaa651d1cdf00993ac01116c08
alex olivares <alexander.b.olivares@gmail.com>
2023-07-19 21:30:54 -0500

    I should switch to sentry

--- winstonLogger.ts
+++ winstonLogger.ts
@@ -1,25 +1,31 @@
 import dotenv from "dotenv";
 import winston from "winston";
 import { WinstonTransport as AxiomTransport } from "@axiomhq/axiom-node";
+import util from "util";
 
 dotenv.config();
 
 const logger = winston.createLogger({
   level: "http",
   format: winston.format.json(),
+  transports: [new AxiomTransport({})],
 });
 
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    })
-  );
-}
+// if (process.env.NODE_ENV !== "production") {
+//   logger.add(
+//     new winston.transports.Console({
+//       format: winston.format.simple(),
+//     })
+//   );
+// }
 
-if (process.env.NODE_ENV === "production") {
-  logger.add(new AxiomTransport({}));
-}
+// if (process.env.NODE_ENV !== "production") {
+//   logger.add(new AxiomTransport({}));
+// }
+
+logger.transports.forEach(t => {
+  console.log(util.inspect(t, { depth: null }));
+});
 
 logger.error({
   level: "error",
