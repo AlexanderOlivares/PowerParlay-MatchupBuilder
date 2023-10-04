b85bc809fd1c3dcf2a46321b237e41a128c3a8b1
alex olivares <alexander.b.olivares@gmail.com>
2023-10-04 14:22:09 -0500

    add 'Not Started' to match statuses

--- utils/leagueMap.ts
+++ utils/leagueMap.ts
@@ -22,7 +22,7 @@ export const leagueLookup: LeagueLookup = {
 
 export const MATCHUP_STATUSES = {
   // as explained here https://www.thesportsdb.com/forum_topic.php?t=5236
-  notStarted: ["NS"],
+  notStarted: ["NS", "Not Started"],
   gameFinished: ["FT", "AOT", "AET", "PEN", "Match Finished", "AP"],
   shouldPush: ["POST", "PST", "SUSP", "CANC", "ABD", "AWD", "WO", "INTR", "INT"],
 };
